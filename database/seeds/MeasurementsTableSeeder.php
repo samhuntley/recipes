d09b4d3e288c17c6b084bb888f52a6ace8cc95d3
Sam Huntley <sam.huntley@gmail.com>
2017-05-25 16:23:58 -0400

    seeding measurements db table

--- database/seeds/MeasurementsTableSeeder.php
+++ database/seeds/MeasurementsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class MeasurementsTableSeeder extends Seeder
 {
@@ -11,6 +12,154 @@ class MeasurementsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+      $created_at = Carbon::now()->format('Y-m-d H:i:s');
+
+      DB::table('measurements')->insert([
+        'name' => 'oz',
+        'type' => 'weight',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'lb',
+        'type' => 'weight',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'mg',
+        'type' => 'weight',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'g',
+        'type' => 'weight',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'kg',
+        'type' => 'weight',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+
+
+      DB::table('measurements')->insert([
+        'name' => 'tsp',
+        'type' => 'volume',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'tbsp',
+        'type' => 'volume',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'fl oz',
+        'type' => 'volume',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'gill',
+        'type' => 'volume',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'cup',
+        'type' => 'volume',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'pint',
+        'type' => 'volume',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'quart',
+        'type' => 'volume',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'gallon',
+        'type' => 'volume',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'ml',
+        'type' => 'volume',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'l',
+        'type' => 'volume',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'dl',
+        'type' => 'volume',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+
+
+      /*** Length ***/
+      DB::table('measurements')->insert([
+        'name' => 'mm',
+        'type' => 'length',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'cm',
+        'type' => 'length',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'm',
+        'type' => 'length',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'in',
+        'type' => 'length',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => 'ft',
+        'type' => 'length',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+
+      /*** Temperature ***/
+      DB::table('measurements')->insert([
+        'name' => '°F',
+        'type' => 'temperature',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+      DB::table('measurements')->insert([
+        'name' => '°C',
+        'type' => 'temperature',
+        'created_at' => $created_at,
+        'updated_at' => $created_at,
+      ]);
+
+
     }
 }
