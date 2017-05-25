a51ed954d4c2cb71e2473cafc09f7f1ee7fa9058
samhuntley <sam.huntley@gmail.com>
2017-05-25 16:44:05 -0400

    remove ingredients from recipe list

--- resources/views/partials/recipe.blade.php
+++ resources/views/partials/recipe.blade.php
@@ -1,9 +1,4 @@
 <div class="panel-body">
   <h2><a href="/recipes/{{ $recipe->id }}">{{ $recipe->name}}</a></h2>
-  <ul class="list-group ingredients">
-    @foreach ($recipe->ingredients as $ingredient)
-      <li class="list-group-item">{{ $ingredient->name }}</li>
-    @endforeach
-  </ul>
     {{ str_limit($recipe->instructions, 25) }}
 </div>
